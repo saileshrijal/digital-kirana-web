c52f06e0152c38415f92a16be740496d5eedbc29
saileshrijal <saileshrijal1@gmail.com>
2022-09-17 22:54:21 +0545

    dbset added on applicationdbcontext

--- digitalkirana.web/Data/ApplicationDbContext.cs
+++ digitalkirana.web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using digitalkirana.web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace digitalkirana.web.Data;
@@ -9,4 +10,13 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Customer>? Customers { get; set; }
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Purchase>? Purchases { get; set; }
+    public DbSet<PurchaseDetails>? PurchaseDetails { get; set; }
+    public DbSet<Sales>? Sales { get; set; }
+    public DbSet<SalesDetails>? SalesDetails { get; set; }
+    public DbSet<Supplier>? Suppliers { get; set; }
 }
